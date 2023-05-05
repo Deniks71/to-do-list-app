df8a80a1f0c947f47d2aab83ac5d230715b2fada
Deniks71 <denilsonn7187@gmail.com>
2023-05-05 11:29:22 -0300

    criando funções e eventos para add todo

--- js/script.js
+++ js/script.js
@@ -3,22 +3,33 @@ let addButton = document.querySelector(".button");
 let todoContainer = document.querySelector(".todolist-list-tasks");
 
 
+const saveTodo = (text) => {
+     // creating div for li , button and icon
+     const todo = document.createElement('div');
+     todo.classList.add('todolist-list-item');
+     
+     // creating li
+     let list = document.createElement('li');
+     list.classList.add('list-style'); 
+     list.innerText = text;
+     //list.innerText = input.value;
+     todo.appendChild(list);
+
+     //creating button
+    let deleteItem = document.createElement('button');
+    deleteItem.classList.add('delete-btn');
+    deleteItem.innerHTML = '<i class="fa-solid fa-delete-left">';
+    todo.appendChild(deleteItem);
+    todoContainer.appendChild(todo);
+
+}
+
 addButton.addEventListener('click', () => {
-    let list = document.createElement('li');
-    list.classList.add('list-style')
-    list.innerText = input.value;
-    todoContainer.appendChild(list);
-
-    let deleteItem = document.createElement('i');
-    deleteItem.classList.add('fa-solid');
-    deleteItem.classList.add('fa-delete-left');
-    todoContainer.appendChild(deleteItem);
-    
-
+    saveTodo(input.value);
+    //inserindo lista e button dentro da div e após isso div dentro da ul.
+    //todoContainer.appendChild(list);
     input.value = "";
 
-    deleteItem.addEventListener('click', (evento) => {
-        
-
-    })
 })
+// ESTUDAR TARGET ELEMENT PARA PROSSEGUIR COM OS PROXIMOS EVENTOS! CONSULTAR VIDEO DO  MATHEYS BATTISTI
+
