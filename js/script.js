9177bd1f6cf77d70b0f04cb4c13d3dd4e0785a56
Denilson Nascimento Santos.T_SSI <denilson.santos.T_SSI@grupoboticario.com.br>
2023-05-04 21:22:56 -0300

    remove child

--- js/script.js
+++ js/script.js
@@ -2,11 +2,23 @@ let input = document.querySelector(".todolist-cadastro-input");
 let addButton = document.querySelector(".button");
 let todoContainer = document.querySelector(".todolist-list-tasks");
 
+
 addButton.addEventListener('click', () => {
     let list = document.createElement('li');
     list.classList.add('list-style')
     list.innerText = input.value;
     todoContainer.appendChild(list);
 
+    let deleteItem = document.createElement('i');
+    deleteItem.classList.add('fa-solid');
+    deleteItem.classList.add('fa-delete-left');
+    todoContainer.appendChild(deleteItem);
+    
+
     input.value = "";
+
+    deleteItem.addEventListener('click', (evento) => {
+        
+
+    })
 })
